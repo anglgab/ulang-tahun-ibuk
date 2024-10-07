11a826df040491021a5de53e75e50e8c9723359e
anglgab <Ilhamkurnia334@gmail.com>
2024-10-07 11:28:28 +0700

    Add files via upload

---
+++ UltahIbook (1)/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  document.getElementById("gambar").src = "./image/birthday-3148707_1280.png";
+  document.getElementById("text").innerHTML = "hallo ibuuukk, giman akabarnya     sehat kan ya buuk?... sehat selalu yaaa iki program percobaan e ilham gawe     website eheh, egen kurang maximal tapi ilham bakalan tetep belajar, oh iya     selamat ulang tahun ya buk  yang ke 43 sehat selalu buat ibuk yaaa";
+  document
+}
+
+function myFunction2() {
+  document.getElementById
+}
+
+
+
+  
+   
